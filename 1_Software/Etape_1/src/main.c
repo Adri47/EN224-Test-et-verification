49a23364d2a8fab4a76e1a2b3d875e9729ac3569
Adri47 <adrien.clain47@gmail.com>
2021-02-09 08:43:25 +0100

    Etape 1 ok

--- 1_Software/Etape_1/src/main.c
+++ 1_Software/Etape_1/src/main.c
@@ -5,12 +5,27 @@
 int PGCD(int A, int B)
 {
 
+		while(A != B)
+		{
+			if (A >B)
+			{
+				A = A - B;
+
+			}else{
+				B = B - A;
+			}
+		}
 	return A;
 }
 
 int main (int argc, char * argv []){
 	printf("(II) Starting PGCD program\n");
 
+	if (argc > 1){
+	int A = atoi(argv[1]);
+	int B = atoi(argv[2]);
+	printf("PGCD = %d\n", PGCD(A, B));
+}
 
 	printf("(II) End of PGCD program\n");
   return 0;
