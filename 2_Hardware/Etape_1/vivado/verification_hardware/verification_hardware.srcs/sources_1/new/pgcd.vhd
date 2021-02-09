f9b6bc429b9fb6cf6ce4c5133bb353047a1597d2
Adri47 <adrien.clain47@gmail.com>
2021-02-09 15:33:42 +0100

    test

---
+++ 2_Hardware/Etape_1/vivado/verification_hardware/verification_hardware.srcs/sources_1/new/pgcd.vhd
@@ -0,0 +1,33 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+ENTITY PGCD IS
+PORT ( 
+	CLK      : in  STD_LOGIC;
+	RESET    : in  STD_LOGIC;
+
+	idata_a  : in  STD_LOGIC_VECTOR (31 downto 0);
+	idata_b  : in  STD_LOGIC_VECTOR (31 downto 0);
+	idata_en : in  STD_LOGIC;
+
+	odata    : out STD_LOGIC_VECTOR (31 downto 0);
+	odata_en : out STD_LOGIC
+);
+END PGCD;
+
+architecture Behavioral of pgcd is
+
+begin
+
+
+end Behavioral;
